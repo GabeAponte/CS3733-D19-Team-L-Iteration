9ed01fbd9d9503785af6d8c55160f17fe4d29cbf
PJ-Mara <pjmarawork@gmail.com>
2019-03-25 09:26:54 -0400

    Updated location object

--- src/main/java/EditTableController.java
+++ src/main/java/EditTableController.java
@@ -27,10 +27,9 @@ public class EditTableController {
     public void initialize(){
         table.setEditable(true);
 
-        PrototypeLocation test1 = new PrototypeLocation(1, "Elevator");
-        PrototypeLocation test2 = new PrototypeLocation(2, "Coffee");
-        PrototypeLocation test3 = new PrototypeLocation(1, "Elevator");
-        final ObservableList<PrototypeLocation> data = FXCollections.observableArrayList(test1, test2, test3, test1, test1, test2, test1, test2, test3, test1, test1, test2);
+        PrototypeLocation test1 = new PrototypeLocation("1", 2, 2, 2, "Main", "elevator", "second floor elevator", "Elevator2");
+        //PrototypeLocation test2 = new PrototypeLocation("1", "Coffee");
+        final ObservableList<PrototypeLocation> data = FXCollections.observableArrayList(test1);
 
         idCol.setCellValueFactory(new PropertyValueFactory<PrototypeLocation,Integer>("id"));
         nameCol.setCellValueFactory(new PropertyValueFactory<PrototypeLocation,String>("name"));
--- src/main/java/PrototypeLocation.java
+++ src/main/java/PrototypeLocation.java
@@ -1,25 +1,82 @@
 public class PrototypeLocation {
 
-    private int id;
-    private String name;
+    private int xcoord, ycoord, floor;
+    private String id, building, nodeType, longName, shortName;
 
-    public PrototypeLocation(int idIn, String nameIn) {
-        name = nameIn;
+
+    public PrototypeLocation(String idIn, int xcoordIn, int ycoordIn, int floorIn, String buildingIn, String nodeTypeIn,
+                             String longNameIn, String shortNameIn) {
         id = idIn;
+        xcoord = xcoordIn;
+        ycoord = ycoordIn;
+        floor=  floorIn;
+        building = buildingIn;
+        nodeType = nodeTypeIn;
+        longName = longNameIn;
+        shortName = shortNameIn;
     }
 
-    public int getId() {
+    public int getXcoord() {
+        return xcoord;
+    }
+
+    public void setXcoord(int xcoord) {
+        this.xcoord = xcoord;
+    }
+
+    public int getYcoord() {
+        return ycoord;
+    }
+
+    public void setYcoord(int ycoord) {
+        this.ycoord = ycoord;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getBuilding() {
+        return building;
     }
 
-    public void setName(String id) { this.name = name;}
+    public void setBuilding(String building) {
+        this.building = building;
+    }
 
+    public String getNodeType() {
+        return nodeType;
+    }
+
+    public void setNodeType(String nodeType) {
+        this.nodeType = nodeType;
+    }
+
+    public String getLongName() {
+        return longName;
+    }
+
+    public void setLongName(String longName) {
+        this.longName = longName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
 }
