c18ec55131daa4bf8ea4ae204468ce8ff8d5e752
ifeeney <ifeeney@wpi,edu>
2019-03-28 17:57:48 -0400

    added Suggestion Box Controller

---
+++ src/main/java/SuggestionBoxController.java
@@ -0,0 +1,13 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class SuggestionBoxController {
+
+    @FXML
+    private Button submitFeedback;
+
+    @FXML
+    private TextArea feedbackComments;
+
+}
