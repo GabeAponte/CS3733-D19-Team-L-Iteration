2cf3209b53301c8436ff952fd5b0a01dfdaeb2a7
GabeAponte <36209249+GabeAponte@users.noreply.github.com>
2019-03-31 18:03:00 -0400

    basic errors for book room screen

--- src/main/java/BookRoomController.java
+++ src/main/java/BookRoomController.java
@@ -1,17 +1,19 @@
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTimePicker;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class BookRoomController {
 
@@ -20,6 +22,9 @@ public class BookRoomController {
 
     @FXML
     private ChoiceBox avaliableRooms;
+
+    @FXML
+    private Label error;
 
     @FXML
     private JFXButton requestRoom;
@@ -44,4 +49,44 @@ public class BookRoomController {
         Scene scene = new Scene(root);
         thestage.setScene(scene);
     }
+
+    //LocalTime startTimeValue = null;
+
+    @FXML
+    private void findRoom(ActionEvent event) {
+
+
+        LocalTime startTimeValue = startTime.getValue();
+        LocalTime endTimeValue = endTime.getValue();
+        LocalDate roomDate = datePicker.getValue();
+
+        if (startTimeValue == null && endTimeValue == null && roomDate == null) {
+            error.setText("Please select start and end times and a date");
+        }
+
+        else if (startTimeValue == null && endTimeValue == null) {
+            error.setText("Please select start and end times");
+        }
+
+        else if (startTimeValue == null && roomDate == null) {
+            error.setText("Please select a start time and a date");
+        }
+
+        else if (endTimeValue == null && roomDate == null) {
+            error.setText("Please select an end time and a date");}
+
+        else if (startTimeValue == null) {
+            error.setText("Please select a start time");
+        }
+        else if (endTimeValue == null) {
+            error.setText("Please select an end time");
+
+        }else if (startTimeValue == endTimeValue) {
+            error.setText("Times cannot be the same");
+
+        } else {
+            error.setText("test");
+        }
+    }
+
 }
