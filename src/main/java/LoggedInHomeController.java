4499a414f3e60461ccf15ccdcec55b8ea485a5ee
ifeeney <ifeeney@wpi,edu>
2019-03-31 19:29:26 -0400

    Added LoggedInController

--- src/main/java/LoggedInHomeController.java
+++ src/main/java/LoggedInHomeController.java
@@ -1,2 +1,42 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoggedInHomeController {
+
+    private Stage thestage;
+
+    @FXML
+    private Button fufillServiceRequest;
+
+    @FXML
+    private Button logOut;
+
+    @FXML
+    private Button bookRoom;
+
+    @FXML
+    private Button editLocations;
+
+    @FXML
+    private Button findPath;
+
+    @FXML
+    private Button serviceRequest;
+
+    @FXML
+    private void backPressed() throws IOException {
+        thestage = (Stage) logOut.getScene().getWindow();
+        AnchorPane root;
+        root = FXMLLoader.load(getClass().getResource("HospitalHome.fxml"));
+        Scene scene = new Scene(root);
+        thestage.setScene(scene);
+    }
+
 }
