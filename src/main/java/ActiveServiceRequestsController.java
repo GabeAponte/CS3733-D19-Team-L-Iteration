b0c7db32b451a280ef0ec82efe0f14264fd09ddd
GabeAponte <36209249+GabeAponte@users.noreply.github.com>
2019-04-01 16:59:19 -0400

    fullfill part done

--- src/main/java/ActiveServiceRequestsController.java
+++ src/main/java/ActiveServiceRequestsController.java
@@ -1,12 +1,17 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ActiveServiceRequestsController {
 
@@ -15,6 +20,18 @@ public class ActiveServiceRequestsController {
 
     @FXML
     private Button back;
+
+    @FXML
+    TableColumn<ServiceRequestTable,String> requestDepartment;
+
+    @FXML
+    TableColumn<ServiceRequestTable, String> assignedEmployee;
+
+    @FXML
+    TableColumn<ServiceRequestTable, String> fullfilled;
+
+    @FXML
+    TableColumn<ServiceRequestTable, String> comment;
 
     @FXML
     private TableView activeRequests;
@@ -33,6 +50,32 @@ public class ActiveServiceRequestsController {
         Scene scene = new Scene(root);
         thestage.setScene(scene);
     }
+
+
+    public void initialize(){
+        activeRequests.setEditable(false);
+       // this.makeEditable.setDisable(true);
+        final ObservableList<ServiceRequestTable> data = FXCollections.observableArrayList();
+        ServiceRequestAccess sr = new ServiceRequestAccess();
+
+        int count;
+        count  = 0;
+        while(count < sr.countRecords()){
+            ArrayList<String> arr= sr.getRequests(count);
+            ServiceRequestTable testx = new ServiceRequestTable(arr.get(0), arr.get(1),arr.get(2), arr.get(3), arr.get(4));
+            count++;
+            data.add(testx);
+        }
+
+
+        requestDepartment.setCellValueFactory(new PropertyValueFactory<ServiceRequestTable,String>("requestDepartment"));
+        comment.setCellValueFactory(new PropertyValueFactory<ServiceRequestTable, String>("comment"));
+        assignedEmployee.setCellValueFactory(new PropertyValueFactory<ServiceRequestTable, String>("assignedEmployee"));
+        fullfilled.setCellValueFactory(new PropertyValueFactory<ServiceRequestTable, String>("fullfilled"));
+        //noinspection CodeBlock2Expr
+        //activeRequests.setItems(data);
+    }
+
 }
 //TODO: Add database functionality to populate service request table and select one to go to fulfill screen
        /* @FXML
