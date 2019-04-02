cbdc70407675ad61958d7d6419771b8a2231ebb3
nwalzer <nswalzer@gmail.com>
2019-04-02 19:31:30 -0400

    HHHHHHHHHHHHHHHHHHHH

--- src/main/java/LogInController.java
+++ src/main/java/LogInController.java
@@ -1,5 +1,4 @@
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,11 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import sun.rmi.runtime.Log;
 
 import java.io.IOException;
 
