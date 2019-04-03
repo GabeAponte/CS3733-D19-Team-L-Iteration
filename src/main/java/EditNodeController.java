e81781e1dce99e0f0463962e531f4e8521fc2121
nwalzer <nswalzer@gmail.com>
2019-04-02 20:12:03 -0400

    done please

--- src/main/java/EditNodeController.java
+++ src/main/java/EditNodeController.java
@@ -127,12 +127,15 @@ public class EditNodeController {
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("EditEdges.fxml"));
                 roots = loader.load();
                 EditEdgesController scene2Controller = loader.getController();
+                scene2Controller.init(uname);
                 scene2Controller.populateNodeList(locationIDS);
                 scene2Controller.flipBool();
                 scene2Controller.setInitialValues(nodeID.getText(), "ADD EDGE");
             } else {
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("EditLocation.fxml"));
                 roots = loader.load();
+                EditLocationController scene2Controller = loader.getController();
+                scene2Controller.init(uname);
             }
             Scene scene = new Scene(roots);
             thestage.setScene(scene);
