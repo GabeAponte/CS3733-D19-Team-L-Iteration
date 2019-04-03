2cb6e5e4897f7e38d4abb0616cee48afc9b06d04
nwalzer <nswalzer@gmail.com>
2019-04-02 20:23:52 -0400

    changed an init to public

--- src/main/java/ActiveServiceRequestsController.java
+++ src/main/java/ActiveServiceRequestsController.java
@@ -42,7 +42,7 @@ public class ActiveServiceRequestsController {
 
     @SuppressWarnings("Duplicates")
 
-    void init(String username){
+    public void init(String username){
         uname = username;
         init2();
     }
