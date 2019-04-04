6b4dd3b1d0b31af0424a8ff20286e9c24f42a11b
Alex <abell@wpi.edu>
2019-04-04 18:31:15 -0400

    Removed Comments

--- src/main/java/PanAndZoomPane.java
+++ src/main/java/PanAndZoomPane.java
@@ -6,43 +6,6 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 
-//public class ZoomablePane extends AnchorPane {
-//
-//    final double SCALE_DELTA = 1.1;
-//    double currentZoom;
-//
-//    public Group content = new Group();
-//
-//    public ZoomablePane() {
-//        super();
-//        currentZoom = 1.0;
-//        getChildren().add(content);
-//        content.setAutoSizeChildren(true);
-////        setOnScroll((ScrollEvent event) -> {
-////            event.consume();
-////            if (event.getDeltaY() == 0 || (event.getDeltaY() < 0 && currentZoom <= 1.0001)) {
-////                return;
-////            }
-////
-////            double scaleFactor
-////                    = (event.getDeltaY() > 0)
-////                    ? SCALE_DELTA
-////                    : 1 / SCALE_DELTA;
-////
-////            content.setScaleX(content.getScaleX() * scaleFactor);
-////            content.setScaleY(content.getScaleY() * scaleFactor);
-////            currentZoom = currentZoom * scaleFactor;
-////
-////            System.out.println(currentZoom);
-////        });
-//    }
-//
-//
-//    public void addChildren(Node n){
-//        content.getChildren().add(n);
-//    }
-//}
-
 public class PanAndZoomPane extends Pane {
 
     public static final double DEFAULT_DELTA = 1.2d;
