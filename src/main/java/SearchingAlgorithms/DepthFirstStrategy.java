ebc3862a6e9cf94e819e6ee001bd4e079aaec53c
PJ-Mara <pjmarawork@gmail.com>
2019-04-05 16:19:39 -0400

    Made templates

--- src/main/java/SearchingAlgorithms/DepthFirstStrategy.java
+++ src/main/java/SearchingAlgorithms/DepthFirstStrategy.java
@@ -1,4 +1,21 @@
 package SearchingAlgorithms;
+import Object.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class DepthFirstStrategy {
+public class DepthFirstStrategy implements PathfindingStrategy{
+
+    private HashMap<String, Location> lookup;
+
+    public DepthFirstStrategy(HashMap<String, Location> hash) {
+        this.lookup = hash;
+        //this.start = start;
+        //this.end = end;
+    }
+
+    public Path findPath(Location start, Location end) {
+        ArrayList<Location> path = new ArrayList<Location>();
+        Path p = new Path(path);
+        return p;
+    }
 }
