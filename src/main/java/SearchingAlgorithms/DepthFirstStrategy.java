868d9e32bb1323349ef9365649f0b7f25a15bf90
lilezhang <lzhang10@wpi.edu>
2019-04-05 17:40:18 -0400

    Begin DepthFirstStrategy

--- src/main/java/SearchingAlgorithms/DepthFirstStrategy.java
+++ src/main/java/SearchingAlgorithms/DepthFirstStrategy.java
@@ -1,4 +1,5 @@
 package SearchingAlgorithms;
+import Access.EdgesAccess;
 import Object.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,9 +16,24 @@ public class DepthFirstStrategy implements PathfindingStrategy{
 
     //Larry - Using Depth First Search (DFS) to find the path from given start position to end position
     public Path findPath(Location start, Location end) {
+        EdgesAccess ea = new EdgesAccess();
         ArrayList<Location> path = new ArrayList<Location>();
+        Path p = new Path(path);
+        if(start == end)
+        {
+            p.addToPath(start);
+            return p;
+        }
+
         path.add(start);
+        start.setParentID("START");
+        ArrayList<String> tempList = new ArrayList<String>();
+        tempList = (ea.getConnectedNodes(start.getLocID()));
         
+
+
+
+
 
 
         Path p = new Path(path);
