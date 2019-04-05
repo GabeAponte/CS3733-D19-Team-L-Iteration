5a53e23fb1ea49a20062970766959a4213422b8c
Daniel <djalvarado@wpi.edu>
2019-04-05 18:20:29 -0400

    Database with all nodes and edges I'm working on fixing errors with the new database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,14 +25,10 @@ public class Main extends Application {
         ReservationAccess ra = new ReservationAccess();
         ServiceRequestAccess sra = new ServiceRequestAccess();
         SuggestionBasicAccess sba = new SuggestionBasicAccess();
-       // na.deleteRecords();
-       // ea.deleteRecords();
-        ra.deleteRecords();
-        sra.deleteRecords();
-        sba.deleteRecords();
+        ea.deleteRecords();
 
-       // na.readCSVintoTable();
-       // ea.readCSVintoTable();
+        na.readCSVintoTable();
+        ea.readCSVintoTable();
         launch(args);
     }
 }
