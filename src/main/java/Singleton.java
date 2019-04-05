ef5e6653b9343eb9f0e3b848b46e4dcb25c9c9cf
nchintada <nchintada@wpi.edu>
2019-04-05 18:29:17 -0400

    Filled the Singleton class so we can start implementing it.

--- src/main/java/Singleton.java
+++ src/main/java/Singleton.java
@@ -1,3 +1,41 @@
 public class Singleton {
 
+    private static boolean loggedIn;
+    private static String username;
+    private static int num;
+
+    private static Singleton single = new Singleton();
+    private Singleton(){
+        loggedIn = false;
+        username = "";
+        num = 0;
+    }
+
+    public static Singleton getInstance() {
+        return single;
+    }
+
+    public static boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public static void setLoggedIn(boolean loggedIn) {
+        Singleton.loggedIn = loggedIn;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        Singleton.username = username;
+    }
+
+    public static int getNum() {
+        return num;
+    }
+
+    public static void setNum(int num) {
+        Singleton.num = num;
+    }
 }
