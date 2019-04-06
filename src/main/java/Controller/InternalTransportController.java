46bce0268e7a452afe499575188c22a64f630911
nwalzer <nswalzer@gmail.com>
2019-04-05 21:26:04 -0400

    start internal transport

--- src/main/java/Controller/InternalTransportController.java
+++ src/main/java/Controller/InternalTransportController.java
@@ -1,5 +1,30 @@
 package Controller;
 
+import Object.*;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
 public class InternalTransportController {
+    @FXML
+    JFXComboBox<Location> startBox;
+
+    @FXML
+    JFXComboBox<Location> endBox;
+
+    @FXML
+    JFXTextField phoneField;
+
+    @FXML
+    JFXTextField typeField;
+
+    @FXML
+    JFXTextArea commentBox;
+
+    public void initialize(){
+        //do something
+    }
+
 
 }
