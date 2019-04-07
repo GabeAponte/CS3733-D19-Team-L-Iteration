5268d0d99424ffcb1aff7bd77077cb1634a1db40
GabeAponte <36209249+GabeAponte@users.noreply.github.com>
2019-04-07 11:55:58 -0400

    Create and Edit account screen and a basic controller with TODO list

---
+++ src/main/java/Controller/CreateEditAccountController.java
@@ -0,0 +1,81 @@
+package Controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXRadioButton;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CreateEditAccountController {
+
+    private Stage thestage;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private Button submit;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField confrimPassword;
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField employeeID;
+
+    @FXML
+    private TextField nickname;
+
+    @FXML
+    private JFXComboBox position;
+
+    @FXML
+    private JFXRadioButton isAdmin;
+
+    @FXML
+    private Label errorLabel;
+
+    @SuppressWarnings("Duplicates")
+    @FXML
+    private void backPressed() throws IOException {
+        thestage = (Stage) back.getScene().getWindow();
+        AnchorPane root;
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("LoggedInHome.fxml"));
+        Scene scene = new Scene(root);
+        thestage.setScene(scene);
+
+        //TODO: Gray out submit button if not everything is filled in.
+        // If admin users select create account on previous page, have the title label reflect that
+        // If employee clicks on their account, have the title label reflect that
+        // employeeID can't be change
+        // Only admins can edit position, employee ID and isAdmin
+        // email must contain @ and .domain
+        // username must be unique
+        // add error cases for the error label
+        // have the new employee information be updated or created in the database when submit button is pressed
+    }
+}
