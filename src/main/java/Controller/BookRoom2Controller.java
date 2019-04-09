d15cd8eb180fd89e74fd20c9e124e9d1e5da3169
Isabella Feeney <ifeeney@wpi.edu>
2019-04-08 21:49:11 -0400

    created room object

--- src/main/java/Controller/BookRoom2Controller.java
+++ src/main/java/Controller/BookRoom2Controller.java
@@ -92,4 +92,6 @@ public class BookRoom2Controller {
 
     }
 
+
+
 }
---
+++ src/main/java/Object/Room.java
@@ -0,0 +1,18 @@
+package Object;
+
+public class Room{
+
+    String time;
+    boolean class1;
+    boolean class2;
+    boolean class3;
+    boolean class4;
+    boolean class5;
+    boolean class6;
+    boolean class7;
+    boolean class8;
+    boolean class9;
+    boolean auditorium;
+
+
+}
