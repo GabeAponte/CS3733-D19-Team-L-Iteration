d1047624a1e98abf20ba5ab2ebacee886819b3f6
Isabella Feeney <ifeeney@wpi.edu>
2019-04-14 23:28:38 -0400

    some things work, but not all

--- src/main/java/edu/wpi/cs3733/d19/teamL/RoomBooking/BookRoomController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamL/RoomBooking/BookRoomController.java
@@ -16,11 +16,13 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -75,19 +77,42 @@ public class BookRoomController {
     private Button bookRoomBack;
 
     Timeline timeout;
+    private boolean firstTimeRan = true;
 
     final ObservableList<String> listOfRooms = FXCollections.observableArrayList();
     ArrayList<String> rooms = new ArrayList<>();
-    //ArrayList<ArrayList<String>> myLocations = new ArrayList<ArrayList<String>>();
+    ArrayList<String> allRooms = new ArrayList<String>();
+    ArrayList<Boolean> availRooms = new ArrayList<Boolean>();
     private SceneGestures sceneGestures;
-    private ArrayList<Circle> circles = new ArrayList<Circle>();
-    private ArrayList<Polygon> polygons = new ArrayList<Polygon>();
-    private ArrayList<String> myListOfRooms = new ArrayList<String>();
-    private ArrayList<String> reverseListOfRooms = new ArrayList<String>();
+    private ArrayList<RoomDisplay> DisplayRooms = new ArrayList<RoomDisplay>();
 
-    public void initialize(){
+    public void initialize() {
+
+        double room1[] = {};
+        double room2[] = {};
+        double room3[] = {};
+        double room4[] = {2420, 180, 2610, 180, 2730, 540, 2420, 540};
+        double room5[] = {};
+        double room6[] = {2090, 180, 2410, 180, 2410, 540, 2090, 540};
+        double room7[] = {};
+        double room8[] = {1750, 180, 2080, 180, 2080, 540, 1750, 540};
+        double room9[] = {1775, 710, 1840, 710, 1840, 750, 1980, 750, 1980, 930, 1775, 930};
+        double auditorium[] = {};
+
+        DisplayRooms.add(new RoomDisplay("CLassroom 1 (Computer)", room1));
+        DisplayRooms.add(new RoomDisplay("CLassroom 2 (Computer)", room2));
+        DisplayRooms.add(new RoomDisplay("CLassroom 3 (Computer)", room3));
+        DisplayRooms.add(new RoomDisplay("CLassroom 4 (Classroom)", room4));
+        DisplayRooms.add(new RoomDisplay("CLassroom 5 (Computer)", room5));
+        DisplayRooms.add(new RoomDisplay("CLassroom 6 (Classroom)", room6));
+        DisplayRooms.add(new RoomDisplay("CLassroom 7 (Computer)", room7));
+        DisplayRooms.add(new RoomDisplay("CLassroom 8 (Classroom)", room8));
+        DisplayRooms.add(new RoomDisplay("CLassroom 9 (Computer)", room9));
+        DisplayRooms.add(new RoomDisplay("Mission Hall Auditorium", auditorium));
+
         roomImage.fitWidthProperty().bind(imagePane.widthProperty());
         roomImage.fitHeightProperty().bind(imagePane.heightProperty());
+
         startTime.setValue(LocalTime.now());
         endTime.setValue(LocalTime.now().plusHours(1));
         datePicker.setValue(LocalDate.now());
@@ -97,8 +122,8 @@ public class BookRoomController {
 
             @Override
             public void handle(ActionEvent event) {
-                if((System.currentTimeMillis() - single.getLastTime()) > single.getTimeoutSec()){
-                    try{
+                if ((System.currentTimeMillis() - single.getLastTime()) > single.getTimeoutSec()) {
+                    try {
                         single.setLastTime();
                         single.setDoPopup(true);
                         single.setLoggedIn(false);
@@ -117,7 +142,7 @@ public class BookRoomController {
                         Scene newScene = new Scene(sceneMain);
                         thisStage.setScene(newScene);
                         timeout.stop();
-                    } catch (IOException io){
+                    } catch (IOException io) {
                         System.out.println(io.getMessage());
                     }
                 }
@@ -127,7 +152,8 @@ public class BookRoomController {
 
         timeout.setCycleCount(Timeline.INDEFINITE);
         timeout.play();
-        fieldsEntered();
+        //fieldsEntered();
+
     }
 
     @FXML
@@ -135,7 +161,7 @@ public class BookRoomController {
         timeout.stop();
         Singleton single = Singleton.getInstance();
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("EmployeeLoggedInHome.fxml"));
-        if(single.isIsAdmin()){
+        if (single.isIsAdmin()) {
             loader = new FXMLLoader(getClass().getClassLoader().getResource("AdminLoggedInHome.fxml"));
         }
         Parent sceneMain = loader.load();
@@ -145,7 +171,7 @@ public class BookRoomController {
     }
 
     @FXML
-    private void switchToTable() throws IOException{
+    private void switchToTable() throws IOException {
         timeout.stop();
         Singleton single = Singleton.getInstance();
         single.setLastTime();
@@ -192,7 +218,8 @@ public class BookRoomController {
 
         //Gabe - error when end time and date are blank
         else if (endTimeValue == null && roomDate == null) {
-            error.setText("Please select an end time and a date.");}
+            error.setText("Please select an end time and a date.");
+        }
 
         //Gabe - error when start time is blank
         else if (startTimeValue == null) {
@@ -207,24 +234,17 @@ public class BookRoomController {
         //Gabe - error when date is blank
         else if (roomDate == null) {
             error.setText("Please select a date.");
-        }
-
-        else if (startTimeValue.equals(endTimeValue)) {
+        } else if (startTimeValue.equals(endTimeValue)) {
             error.setText("Times cannot be the same.");
-        }
-        else if (startTimeValue.compareTo(curTime) < 0 && roomDate.equals(curDate)) {
+        } else if (startTimeValue.compareTo(curTime) < 0 && roomDate.equals(curDate)) {
             error.setText("Please select a current or future time for today.");
-        }
-        else if (roomDate.compareTo(curDate) < 0) {
+        } else if (roomDate.compareTo(curDate) < 0) {
             error.setText("Please select a time for today or a future day.");
-        }
-        else if (availableRooms.getValue() == null) {
+        } else if (availableRooms.getValue() == null) {
             error.setText("Please pick a room.");
-        }
-        else if (startTimeValue.compareTo(endTimeValue) > 0) {
+        } else if (startTimeValue.compareTo(endTimeValue) > 0) {
             error.setText("Start time cannot be ahead of end time.");
-        }
-        else {
+        } else {
             error.setTextFill(Color.WHITE);
             error.setText("Room booked.");
             int startTimeMil = startTime.getValue().getHour() * 100 + startTime.getValue().getMinute();
@@ -240,7 +260,7 @@ public class BookRoomController {
 //                    roomID = rooms.get(i - 1);
 //                }
 //            }
-           // System.out.println("Start Time (MIL): " + startTimeMil + "End Time (MIL): " + endTimeMil);
+            // System.out.println("Start Time (MIL): " + startTimeMil + "End Time (MIL): " + endTimeMil);
             RoomAccess ra = new RoomAccess();
             roomReq.makeReservation(ra.getRoomID(roomName), employeeID, date, date, startTimeMil, endTimeMil);
             fieldsEntered();
@@ -250,7 +270,7 @@ public class BookRoomController {
     //todo checks if fields are null and populates table here
     // RA.getAvailRooms returns list of available rooms
     @FXML
-    public void fieldsEntered(){
+    public void fieldsEntered() {
         Singleton single = Singleton.getInstance();
         single.setLastTime();
         RoomAccess ra = new RoomAccess();
@@ -259,30 +279,15 @@ public class BookRoomController {
         String date = "";
         String endDate = "";
 
-        for (Circle c: circles) {
-            imagePane.getChildren().remove(c);
-        }
-        for (Polygon p: polygons){
-            imagePane.getChildren().remove(p);
-        }
-
-        circles.clear();
-        polygons.clear();
-
-        if(startTime.getValue() != null && endTime != null && datePicker.getValue() != null){
+        if (startTime.getValue() != null && endTime != null && datePicker.getValue() != null) {
             startTimeMil = startTime.getValue().getHour() * 100 + startTime.getValue().getMinute();
             endTimeMil = endTime.getValue().getHour() * 100 + endTime.getValue().getMinute();
             date = datePicker.getValue().toString();
             availableRooms.getSelectionModel().clearSelection();
 
             rooms = ra.getAvailRooms(date, date, startTimeMil, endTimeMil);
-            for(int i = 0; i < rooms.size(); i++){
+            for (int i = 0; i < rooms.size(); i++) {
                 //System.out.println("Available Rooms: " + rooms.get(i));
-            }
-
-            for(int i = 0; i< listOfRooms.size(); i++){
-                reverseListOfRooms.add(i, listOfRooms.get(i));
-                myListOfRooms.add(i, listOfRooms.get(i));
             }
 
             listOfRooms.clear();
@@ -290,9 +295,14 @@ public class BookRoomController {
             //System.out.println("startTimeMil: " + startTimeMil + "\n endTimeMil:" + endTimeMil);
             rooms = ra.getAvailRooms(date, date, startTimeMil, endTimeMil);
 
-            for(int i = 0; i < rooms.size(); i++){
-                listOfRooms.add(rooms.get(i));
-                reverseListOfRooms.remove(rooms.get(i));
+            for(int j = 0; j < rooms.size(); j ++) {
+                listOfRooms.add(rooms.get(j));
+                for (int i = 0; i < DisplayRooms.size(); i++) {
+                    if (DisplayRooms.get(i).roomName.equals(rooms.get(j)))
+                        DisplayRooms.get(i).setAvailable(true);
+                    else
+                        DisplayRooms.get(i).setAvailable(false);
+                }
             }
 
             availableRooms.setItems(listOfRooms);
@@ -301,89 +311,59 @@ public class BookRoomController {
     }
 
     @FXML
-    public void displayAllRooms(){
-        Singleton single = Singleton.getInstance();
-        single.setLastTime();
+    public void displayAllRooms() {
 
-        double room1[] = {};
-        double room2[] = {};
-        double room3[] = {};
-        double room4[] = {2420,180,2610,180,2730,540,2420,540};
-        double room5[] = {};
-        double room6[] = {2090,180,2410,180,2410,540,2090,540};
-        double room7[] = {};
-        double room8[] = {1750,180,2080,180,2080,540,1750,540};
-        double room9[] = {1775,710,1840,710,1840,750,1980,750,1980,930,1775,930};
-        double auditorium[] = {};
-
-        for(int i = 0; i< listOfRooms.size(); i++){
-           // System.out.println("!");
-            if(listOfRooms.get(i).toString().equals("Classroom 1 (Computer)")){
-                makePolygon(room1, true);
-            }else if(listOfRooms.get(i).toString().equals("Classroom 2 (Computer)")){
-                makePolygon(room2, true);
-            }else if(listOfRooms.get(i).toString().equals("Classroom 3 (Computer)")){
-                makePolygon(room3, true);
-            }else if(listOfRooms.get(i).toString().equals("Classroom 4 (Classroom)")){
-                makePolygon(room4, true);
-            }else if(listOfRooms.get(i).toString().equals("Classroom 5 (Computer)")){
-                makePolygon(room5, true);
-            }else if(listOfRooms.get(i).toString().equals("Classroom 6 (Classroom)")){
-                makePolygon(room6, true);
-            }else if(listOfRooms.get(i).toString().equals("Classroom 7 (Computer)")){
-                makePolygon(room7, true);
-            }else if(listOfRooms.get(i).toString().equals("Classroom 8 (Classroom)")){
-                makePolygon(room8, true);
-            }else if(listOfRooms.get(i).toString().equals("Classroom 9 (Computer)")){
-                makePolygon(room9, true);
-            }else if(listOfRooms.get(i).toString().equals("Mission Hall Auditorium")){
-                makePolygon(auditorium, true);
+        if(firstTimeRan) {
+            double scaleRatio = Math.min(roomImage.getFitWidth() / roomImage.getImage().getWidth(), roomImage.getFitHeight() / roomImage.getImage().getHeight());
+            for (int i = 0; i < DisplayRooms.size(); i++) {
+                DisplayRooms.get(i).makePolygon(scaleRatio);
+                DisplayRooms.get(i).getPolygon().setOnMouseClicked(onMouseClickedEventHandler);
+                DisplayRooms.get(i).getPolygon().setVisible(false);
+                imagePane.getChildren().add(DisplayRooms.get(i).getPolygon());
             }
+            firstTimeRan = false;
         }
 
-        for(int i = 0; i< reverseListOfRooms.size(); i++){
-            // System.out.println("!");
-            if(reverseListOfRooms.get(i).toString().equals("Classroom 1 (Computer)")){
-                makePolygon(room1, false);
-            }else if(reverseListOfRooms.get(i).toString().equals("Classroom 2 (Computer)")){
-                makePolygon(room2, false);
-            }else if(reverseListOfRooms.get(i).toString().equals("Classroom 3 (Computer)")){
-                makePolygon(room3, false);
-            }else if(reverseListOfRooms.get(i).toString().equals("Classroom 4 (Classroom)")){
-                makePolygon(room4, false);
-            }else if(reverseListOfRooms.get(i).toString().equals("Classroom 5 (Computer)")){
-                makePolygon(room5, false);
-            }else if(reverseListOfRooms.get(i).toString().equals("Classroom 6 (Classroom)")){
-                makePolygon(room6, false);
-            }else if(reverseListOfRooms.get(i).toString().equals("Classroom 7 (Computer)")){
-                makePolygon(room7, false);
-            }else if(reverseListOfRooms.get(i).toString().equals("Classroom 8 (Classroom)")){
-                makePolygon(room8, false);
-            }else if(reverseListOfRooms.get(i).toString().equals("Classroom 9 (Computer)")){
-                makePolygon(room9, false);
-            }else if(reverseListOfRooms.get(i).toString().equals("Mission Hall Auditorium")){
-                makePolygon(auditorium, false);
+        for (int i = 0; i < DisplayRooms.size(); i++) {
+            if(DisplayRooms.get(i).isAvailable()){
+                DisplayRooms.get(i).getPolygon().setStroke(Color.web("GREEN"));
+                DisplayRooms.get(i).getPolygon().setFill(Color.web("GREEN"));
+            } else {
+                DisplayRooms.get(i).getPolygon().setStroke(Color.web("RED"));
+                DisplayRooms.get(i).getPolygon().setFill(Color.web("RED"));
             }
+            DisplayRooms.get(i).getPolygon().setOpacity(0.3);
+            DisplayRooms.get(i).getPolygon().setVisible(true);
+            System.out.println(DisplayRooms.get(i).getPolygon());
         }
     }
 
-    private void makePolygon(double points[], boolean green){
-        double scaleRatio = Math.min(roomImage.getFitWidth()/roomImage.getImage().getWidth(), roomImage.getFitHeight()/roomImage.getImage().getHeight());
-        for(int i = 0; i < points.length; i++){
-            points[i] = points[i]*scaleRatio;
+    private EventHandler<MouseEvent> onMouseClickedEventHandler = new EventHandler<MouseEvent>() {
+
+        @Override
+        public void handle(MouseEvent event) {
+            displayAllRooms();
+            for (int k = 0; k < DisplayRooms.size(); k++) {
+
+                Point2D mousePress = new Point2D(event.getX(), event.getY());
+                if (DisplayRooms.get(k).p.contains(mousePress)) {
+                    imagePane.getChildren().remove(DisplayRooms.get(k).getPolygon());
+                    availableRooms.getSelectionModel().select(listOfRooms.get(k));
+                    fieldsEntered();
+                    DisplayRooms.get(k).changePolygonColor("BLUE");
+                    imagePane.getChildren().add(DisplayRooms.get(k).getPolygon());
+                    System.out.println("Room" + (DisplayRooms.get(k).getRoomName()));
+                }
+            }
+
         }
-        Polygon p = new Polygon(points);
-        if(green) {
-            p.setStroke(Color.web("GREEN"));
-            p.setFill(Color.web("GREEN"));
-        }else{
-            p.setStroke(Color.web("RED"));
-            p.setFill(Color.web("RED"));
-        }
-        p.setOpacity(0.3);
-        imagePane.getChildren().add(p);
-        polygons.add(p);
-    }
+
+    };
 
 }
+
+
+
+
+
 
