8226969981ecf9eac15f9a483659bed2c6dcf6c8
Daniel <djalvarado@wpi.edu>
2019-04-17 01:39:25 -0400

    THIS IS BROKEN RN

--- src/main/java/edu/wpi/cs3733/d19/teamL/RoomBooking/WeeklyRoom.java
+++ src/main/java/edu/wpi/cs3733/d19/teamL/RoomBooking/WeeklyRoom.java
@@ -1,14 +1,12 @@
 package edu.wpi.cs3733.d19.teamL.RoomBooking;
 
-package edu.wpi.cs3733.d19.teamL.RoomBooking;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Room{
+public class WeeklyRoom{
 
     String time;
     boolean isMonday;
@@ -19,9 +17,10 @@ public class Room{
     boolean isSaturday;
     boolean isSunday;
 
-    public Room(String startTime, String endTime, LocalDate theDate){
-        int milTime = Integer.parseInt(startTime);
-        int endMilTime = Integer.parseInt(endTime);
+    public WeeklyRoom(int startTime, int endTime, LocalDate theDate, String classroomName){
+        RoomAccess ra = new RoomAccess();
+        int milTime = startTime;
+        int endMilTime = endTime;
 
         Date startDate = null;
         try {
@@ -42,6 +41,75 @@ public class Room{
         //  System.out.println(startSimpleDate.format(endDate));
 
         time = startSimpleDate.format(startDate) + " - " + endSimpleDate.format(endDate);
+
+        LocalDate weekDay = theDate;
+        String dayOfWeek = theDate.getDayOfWeek().toString();
+        switch(dayOfWeek){
+            case "SUNDAY":
+                isSunday = ra.checkRoom(startTime, endTime,classroomName, weekDay.toString());
+                isMonday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(1).toString());
+                isTuesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(2).toString());
+                isWednesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(3).toString());
+                isThursday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(4).toString());
+                isFriday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(5).toString());
+                isSaturday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(6).toString());
+                break;
+            case "MONDAY":
+                isSunday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(1).toString());
+                isMonday = ra.checkRoom(startTime, endTime,classroomName, weekDay.toString());
+                isTuesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(1).toString());
+                isWednesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(2).toString());
+                isThursday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(3).toString());
+                isFriday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(4).toString());
+                isSaturday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(5).toString());
+                break;
+            case "TUESDAY":
+                isSunday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(2).toString());
+                isMonday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(1).toString());
+                isTuesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.toString());
+                isWednesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(1).toString());
+                isThursday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(2).toString());
+                isFriday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(3).toString());
+                isSaturday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(4).toString());
+                break;
+            case "WEDNESDAY":
+                isSunday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(3).toString());
+                isMonday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(2).toString());
+                isTuesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(1).toString());
+                isWednesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.toString());
+                isThursday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(1).toString());
+                isFriday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(2).toString());
+                isSaturday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(3).toString());
+                break;
+            case "THURSDAY":
+                isSunday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(4).toString());
+                isMonday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(3).toString());
+                isTuesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(2).toString());
+                isWednesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(1).toString());
+                isThursday = ra.checkRoom(startTime, endTime,classroomName, weekDay.toString());
+                isFriday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(1).toString());
+                isSaturday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(2).toString());
+                break;
+            case "FRIDAY":
+                isSunday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(5).toString());
+                isMonday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(4).toString());
+                isTuesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(3).toString());
+                isWednesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(2).toString());
+                isThursday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(1).toString());
+                isFriday = ra.checkRoom(startTime, endTime,classroomName, weekDay.toString());
+                isSaturday = ra.checkRoom(startTime, endTime,classroomName, weekDay.plusDays(1).toString());
+                break;
+            case "SATURDAY":
+                isSunday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(6).toString());
+                isMonday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(5).toString());
+                isTuesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(4).toString());
+                isWednesday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(3).toString());
+                isThursday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(2).toString());
+                isFriday = ra.checkRoom(startTime, endTime,classroomName, weekDay.minusDays(1).toString());
+                isSaturday = ra.checkRoom(startTime, endTime,classroomName, weekDay.toString());
+                break;
+        }
+
 
         
     }
--- src/main/java/edu/wpi/cs3733/d19/teamL/RoomBooking/WeeklyScheduleController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamL/RoomBooking/WeeklyScheduleController.java
@@ -1,11 +1,13 @@
 package edu.wpi.cs3733.d19.teamL.RoomBooking;
 
+import com.android.dx.gen.Local;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import edu.wpi.cs3733.d19.teamL.HomeScreens.HomeScreenController;
 import edu.wpi.cs3733.d19.teamL.Singleton;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -37,31 +39,33 @@ public class WeeklyScheduleController
     private Button goToBookRoom;
 
     @FXML
-    private TreeTableView<Room> bookedTime;
+    private TreeTableView<WeeklyRoom> bookedTime;
 
     @FXML
-    private TreeTableColumn<Room, String> timeCol;
+    private TreeTableColumn<WeeklyRoom, String> timeCol;
 
     @FXML
-    private TreeTableColumn<Room, Boolean> sunCol;
+    private TreeTableColumn<WeeklyRoom, Boolean> sunCol;
 
     @FXML
-    private TreeTableColumn<Room, Boolean> monCol;
+    private TreeTableColumn<WeeklyRoom, Boolean> monCol;
 
     @FXML
-    private TreeTableColumn<Room, Boolean> tueCol;
+    private TreeTableColumn<WeeklyRoom, Boolean> tueCol;
 
     @FXML
-    private TreeTableColumn<Room, Boolean> wedCol;
+    private TreeTableColumn<WeeklyRoom, Boolean> wedCol;
 
     @FXML
-    private TreeTableColumn<Room, Boolean> thuCol;
+    private TreeTableColumn<WeeklyRoom, Boolean> thuCol;
 
     @FXML
-    private TreeTableColumn<Room, Boolean> friCol;
+    private TreeTableColumn<WeeklyRoom, Boolean> friCol;
 
     @FXML
-    private TreeTableColumn<Room, Boolean> satCol;
+    private TreeTableColumn<WeeklyRoom, Boolean> satCol;
+
+    private TreeItem Root = new TreeItem<>("rootxxx");
 
     Timeline timeout;
 
@@ -116,62 +120,251 @@ public class WeeklyScheduleController
     }
 
     public void checkAvailability(String roomName, LocalDate theDate){
-        String dayOfWeek = "";
-        LocalDate weekDay = theDate;
-        //dayOfWeek = weekDay.getDayOfWeek().toString();
-        dayOfWeek = theDate.getDayOfWeek().toString();
-        switch(dayOfWeek){
-            case "SUNDAY":
-                checkAvailiablityOfWeek(weekDay);
-                break;
-            case "MONDAY":
-                checkAvailiablityOfWeek(weekDay.minusDays(1));
-                break;
-            case "TUESDAY":
-                checkAvailiablityOfWeek(weekDay.minusDays(2));
-                break;
-            case "WEDNESDAY":
-                checkAvailiablityOfWeek(weekDay.minusDays(3));
-                break;
-            case "THURSDAY":
-                checkAvailiablityOfWeek(weekDay.minusDays(4));
-                break;
-            case "FRIDAY":
-                checkAvailiablityOfWeek(weekDay.minusDays(5));
-                break;
-            case "SATURDAY":
-                checkAvailiablityOfWeek(weekDay.minusDays(6));
-                break;
-        }
-    }
-
-    @SuppressWarnings("Duplicates")
-    public void checkAvailiablityOfWeek(LocalDate date){
+        Singleton single = Singleton.getInstance();
+        single.setLastTime();
+        bookedTime.setRoot(null);
+        Root.getChildren().clear();
+        RoomAccess ra = new RoomAccess();
+        LocalDate givenDate = datePicker.getValue();
         int startTime = 0;
         int endTime = 30;
-        for(int i = 0; i < 7; i++) {
-            for (int j = 0; j < 47; j++) {
-                // System.out.println("Start Time: " + startTime + " End Time: " + endTime);
-                //TreeItem<Room> bookedRooms = new TreeItem<Room>(new Room(Integer.toString(startTime), Integer.toString(endTime), ra.getAvailRooms(theDate, theDate, startTime, endTime)));
-                //Root.getChildren().add(bookedRooms);
-                //System.out.println(bookedRooms.getValue().getTime());
-                if (i == 0) {
-                    startTime += 30;
-                    endTime += 70;
-                } else if (i % 2 == 0) {
-                    startTime += 30;
-                    endTime += 70;
-                } else {
-                    startTime += 70;
-                    endTime += 30;
-                }
-                startTime %= 2400;
-                endTime %= 2400;
+        for(int i = 0; i < 47; i++){
+            // System.out.println("Start Time: " + startTime + " End Time: " + endTime);
+            TreeItem<WeeklyRoom> bookedRooms = new TreeItem<Room>(new WeeklyRoom(startTime, endTime, theDate, roomName )));
+            Root.getChildren().add(bookedRooms);
+            //System.out.println(bookedRooms.getValue().getTime());
+            if(i == 33) {
+                // System.out.println("Start Time: " + startTime + "End Time: " +endTime);
             }
-            System.out.println(date.getDayOfWeek().toString());
-            date = date.plusDays(1);
+            if(i == 0){
+                startTime += 30;
+                endTime += 70;
+            }
+            else if(i%2 == 0) {
+                startTime += 30;
+                endTime += 70;
+            }
+            else{
+                startTime +=70;
+                endTime +=30;
+            }
+            startTime %= 2400;
+            endTime %= 2400;
         }
+        //System.out.println("Start Time: " + startTime + " End Time: " + endTime);
+        TreeItem<Room> bookedRooms2 = new TreeItem<Room>(new Room(Integer.toString(startTime), Integer.toString(endTime), ra.getAvailRooms(theDate, theDate, startTime, endTime)));
+        Root.getChildren().add(bookedRooms2);
 
+        //timeCol = new TreeTableColumn<Room, String>("Time");
+        timeCol.setCellValueFactory(cellData -> {
+            if(cellData.getValue().getValue()instanceof Room) {
+                return new ReadOnlyObjectWrapper(cellData.getValue().getValue().getTime());
+            }
+            return new ReadOnlyObjectWrapper(cellData.getValue().getValue());
+        });
+
+        class1Col.setCellValueFactory(cellData -> {
+            if(cellData.getValue().getValue()instanceof Room) {
+                if(cellData.getValue().getValue().isClass1()){
+                    //cellData.getValue().
+                    return new ReadOnlyObjectWrapper(true);
+                }else {
+                    return new ReadOnlyObjectWrapper(false);
+                }
+            }
+            return new ReadOnlyObjectWrapper(cellData.getValue().getValue());
+        });
+
+        class1Col.setCellFactory(column -> {
+            TreeTableCell cell = new TreeTableCell<Room, Boolean>() {
+                //@Override
+                protected void updateItem(Boolean item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || !item){
+                        setText("Occupied");
+                        setStyle("-fx-background-color: red");
+                    } else {
+                        setText("Available");
+                        setStyle("-fx-background-color: green");
+                    }
+                }
+            };
+            return cell;
+        });
+
+        class2Col.setCellValueFactory(cellData -> {
+            if(cellData.getValue().getValue()instanceof Room) {
+                if(cellData.getValue().getValue().isClass2()){
+                    //cellData.getValue().
+                    return new ReadOnlyObjectWrapper(true);
+                }else {
+                    return new ReadOnlyObjectWrapper(false);
+                }
+            }
+            return new ReadOnlyObjectWrapper(cellData.getValue().getValue());
+        });
+
+        class2Col.setCellFactory(column -> {
+            TreeTableCell cell = new TreeTableCell<Room, Boolean>() {
+                //@Override
+                protected void updateItem(Boolean item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || !item){
+                        setText("Occupied");
+                        setStyle("-fx-background-color: red");
+                    } else {
+                        setText("Available");
+                        setStyle("-fx-background-color: green");
+                    }
+                }
+            };
+            return cell;
+        });
+
+        class3Col.setCellValueFactory(cellData -> {
+            if(cellData.getValue().getValue()instanceof Room) {
+                if(cellData.getValue().getValue().isClass3()){
+                    //cellData.getValue().
+                    return new ReadOnlyObjectWrapper(true);
+                }else {
+                    return new ReadOnlyObjectWrapper(false);
+                }
+            }
+            return new ReadOnlyObjectWrapper(cellData.getValue().getValue());
+        });
+
+        class3Col.setCellFactory(column -> {
+            TreeTableCell cell = new TreeTableCell<Room, Boolean>() {
+                //@Override
+                protected void updateItem(Boolean item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || !item){
+                        setText("Occupied");
+                        setStyle("-fx-background-color: red");
+                    } else {
+                        setText("Available");
+                        setStyle("-fx-background-color: green");
+                    }
+                }
+            };
+            return cell;
+        });
+
+        class4Col.setCellValueFactory(cellData -> {
+            if(cellData.getValue().getValue()instanceof Room) {
+                if(cellData.getValue().getValue().isClass4()){
+                    //cellData.getValue().
+                    return new ReadOnlyObjectWrapper(true);
+                }else {
+                    return new ReadOnlyObjectWrapper(false);
+                }
+            }
+            return new ReadOnlyObjectWrapper(cellData.getValue().getValue());
+        });
+
+        class4Col.setCellFactory(column -> {
+            TreeTableCell cell = new TreeTableCell<Room, Boolean>() {
+                //@Override
+                protected void updateItem(Boolean item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || !item){
+                        setText("Occupied");
+                        setStyle("-fx-background-color: red");
+                    } else {
+                        setText("Available");
+                        setStyle("-fx-background-color: green");
+                    }
+                }
+            };
+            return cell;
+        });
+
+        class5Col.setCellValueFactory(cellData -> {
+            if(cellData.getValue().getValue()instanceof Room) {
+                if(cellData.getValue().getValue().isClass5()){
+                    //cellData.getValue().
+                    return new ReadOnlyObjectWrapper(true);
+                }else {
+                    return new ReadOnlyObjectWrapper(false);
+                }
+            }
+            return new ReadOnlyObjectWrapper(cellData.getValue().getValue());
+        });
+
+        class5Col.setCellFactory(column -> {
+            TreeTableCell cell = new TreeTableCell<Room, Boolean>() {
+                //@Override
+                protected void updateItem(Boolean item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || !item){
+                        setText("Occupied");
+                        setStyle("-fx-background-color: red");
+                    } else {
+                        setText("Available");
+                        setStyle("-fx-background-color: green");
+                    }
+                }
+            };
+            return cell;
+        });
+
+        class6Col.setCellValueFactory(cellData -> {
+            if(cellData.getValue().getValue()instanceof Room) {
+                if(cellData.getValue().getValue().isClass6()){
+                    //cellData.getValue().
+                    return new ReadOnlyObjectWrapper(true);
+                }else {
+                    return new ReadOnlyObjectWrapper(false);
+                }
+            }
+            return new ReadOnlyObjectWrapper(cellData.getValue().getValue());
+        });
+
+        class6Col.setCellFactory(column -> {
+            TreeTableCell cell = new TreeTableCell<Room, Boolean>() {
+                //@Override
+                protected void updateItem(Boolean item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || !item){
+                        setText("Occupied");
+                        setStyle("-fx-background-color: red");
+                    } else {
+                        setText("Available");
+                        setStyle("-fx-background-color: green");
+                    }
+                }
+            };
+            return cell;
+        });
+
+        class7Col.setCellValueFactory(cellData -> {
+            if(cellData.getValue().getValue()instanceof Room) {
+                if(cellData.getValue().getValue().isClass7()){
+                    //cellData.getValue().
+                    return new ReadOnlyObjectWrapper(true);
+                }else {
+                    return new ReadOnlyObjectWrapper(false);
+                }
+            }
+            return new ReadOnlyObjectWrapper(cellData.getValue().getValue());
+        });
+
+        class7Col.setCellFactory(column -> {
+            TreeTableCell cell = new TreeTableCell<Room, Boolean>() {
+                //@Override
+                protected void updateItem(Boolean item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || !item){
+                        setText("Occupied");
+                        setStyle("-fx-background-color: red");
+                    } else {
+                        setText("Available");
+                        setStyle("-fx-background-color: green");
+                    }
+                }
+            };
+            return cell;
+        });
     }
 
     @FXML
