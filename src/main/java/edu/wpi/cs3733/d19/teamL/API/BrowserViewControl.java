5d5ad7d2517ee47656f6227ccb4bf1ec89ed07d1
Daniel <djalvarado@wpi.edu>
2019-04-17 08:25:06 -0400

    Changes

--- src/main/java/edu/wpi/cs3733/d19/teamL/API/BrowserViewControl.java
+++ src/main/java/edu/wpi/cs3733/d19/teamL/API/BrowserViewControl.java
@@ -2,12 +2,15 @@ package edu.wpi.cs3733.d19.teamL.API;
 
 import com.teamdev.jxbrowser.chromium.Browser;
 import com.teamdev.jxbrowser.chromium.BrowserPreferences;
+import com.teamdev.jxbrowser.chromium.CookieStorage;
 import com.teamdev.jxbrowser.chromium.javafx.BrowserView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,9 +28,21 @@ public class BrowserViewControl implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        BrowserPreferences preferences = browserView.getBrowser().getPreferences();
-        browserView.getBrowser().loadURL(textField.getText());
+        Browser browser = new Browser();
+        CookieStorage cookies = browser.getCookieStorage();
+        int numberOfDeletedCookies = cookies.deleteAll();
+        cookies.save();
+        BrowserView view = new BrowserView(browser);
+        browser.loadURL(textField.getText());
+        try {
+            Robot robot = new Robot();
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
     }
+
 
     public void loadURL(ActionEvent actionEvent) {
         browserView.getBrowser().loadURL(textField.getText());
