eabb041e10aed08051b4c98d66e0c944bde4e830
Anagha Late <anagha20@hotmail.com>
2019-04-24 09:25:41 -0400

    log in button bug fix solved

--- src/main/java/edu/wpi/cs3733/d19/teamL/HomeScreens/LogInController.java
+++ src/main/java/edu/wpi/cs3733/d19/teamL/HomeScreens/LogInController.java
@@ -108,6 +108,8 @@ public class LogInController {
         }));
         timeout.setCycleCount(Timeline.INDEFINITE);
         timeout.play();
+        login.setDisable(true);
+
     }
 
     @SuppressWarnings("Duplicates")
@@ -134,11 +136,10 @@ public class LogInController {
         Singleton single = Singleton.getInstance();
         single.setLastTime();
 
-        Boolean disable = (username.getText().isEmpty() || username.getText().trim().isEmpty() || password.getText().isEmpty() || password.getText().trim().isEmpty());
-        if (!disable) {
-            login.setDisable(false);
-        } else {
+        if (username.getText().isEmpty() || username.getText().trim().isEmpty() || password.getText().isEmpty() || password.getText().trim().isEmpty()) {
             login.setDisable(true);
+        } else {
+            login.setDisable(false);
         }
     }
 
